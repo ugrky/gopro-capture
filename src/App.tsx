48529639c1116b52b5a5eb20c4d1c410ab5a0489
Ugur Kaya <uggie.the.developer@gmail.com>
2023-11-26 17:42:05 +0100

    new buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <button>Request Stream</button>
+        <button>Start Streaming</button>
+
+      </div>
     </div>
   );
 }
